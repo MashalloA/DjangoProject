e7640f9b4a3b3719b9b76f8fbdd5381ea281d878
MashalloA <mashalloalim07@gmail.com>
2024-12-28 19:21:47 +0600

    ad

--- resipe/urls.py
+++ resipe/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.RecipeListView.as_view(), name='recipe_list'),
+    path('recipe_list/', views.RecipeListView.as_view(), name='recipe_list'),
     path('recipe/<int:pk>/', views.RecipeDetailView.as_view(), name='recipe_detail'),
     path('recipe/new/', views.RecipeCreateView.as_view(), name='recipe_create'),
     path('collections/', views.CollectionListView.as_view(), name='collection_list'),
--- resipe/views.py
+++ resipe/views.py
@@ -5,35 +5,17 @@ from django.urls import reverse_lazy
 from .forms import CollectionForm
 
 
-
 class RecipeListView(ListView):
     model = Recipe
     template_name = 'recipes/recipe_list.html'
     context_object_name = 'recipes'
 
-    def get_queryset(self):
-        query = self.request.GET.get('q')
-        if query:
-            return Recipe.objects.filter(title__icontains=query)
-        return Recipe.objects.all()
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['query'] = self.request.GET.get('q', '')
-        return context
 
 class RecipeDetailView(DetailView):
     model = Recipe
     template_name = 'recipes/recipe_detail.html'
     context_object_name = 'recipe'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        recipe = self.get_object()
-        context['total_calories'] = sum(
-            ingredient.calories for ingredient in recipe.ingredients.all() if ingredient.calories
-        )
-        return context
 
 class RecipeCreateView(CreateView):
     model = Recipe
@@ -47,6 +29,7 @@ class CollectionCreateView(CreateView):
     form_class = CollectionForm
     template_name = 'collections/collection_form.html'
     success_url = reverse_lazy('collection_list')
+
 
 class CollectionListView(ListView):
     model = CollectionForm
